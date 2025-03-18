ac6e175e40f86951f5ee9644642d036b1e8b83ee
SamindiM <kaluarachchisamindi@gmail.com>
2025-03-18 20:45:30 +0530

    edit dashboards

--- admin/src/pages/DashboardAccess/DashboardAccess.jsx
+++ admin/src/pages/DashboardAccess/DashboardAccess.jsx
@@ -5,24 +5,24 @@ import { Link } from 'react-router-dom';
 const Access = () => {
   return (
     <div className="Dash-page">
-      <h1 className="dash-message">Kandurata Hardware Employee Acess</h1>
-      <div className="dbutton-container">
-        <Link to="/logistics">
-          <button className="dnav-button">Logistic Manager</button>
-        </Link>
-        <Link to="/dashboard/cashier">
-          <button className="dnav-button">Cashier Dashboard</button>
-        </Link>
-        <Link to="/hr">
-          <button className="dnav-button">HR Manager</button>
-        </Link>
-        <Link to="/employee">
-          <button className="dnav-button">Employee Dashboard</button>
-        </Link>
-       
+      <h1 className="dash-message">Furni Heaven Employee Access</h1>
+      <div className="transparent-box">
+        <div className="dbutton-container">
+          <Link to="/logistics">
+            <button className="dnav-button">Logistic Manager</button>
+          </Link>
+          <Link to="/dashboard/cashier">
+            <button className="dnav-button">Cashier Dashboard</button>
+          </Link>
+          <Link to="/hr">
+            <button className="dnav-button">HR Manager</button>
+          </Link>
+          <Link to="/employee">
+            <button className="dnav-button">Employee Dashboard</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
-
 export default Access;
